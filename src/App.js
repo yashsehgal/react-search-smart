e7265f11c5ef9018a524bfa3f51a2787c3cf9381
Yash Sehgal <yashsehgal.work@gmail.com>
2021-12-29 11:03:27 +0000

    feat: search functionality added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import React from "react";
 import ReactSearchSmart from "./component/ReactSearchSmart";
+import { FaSearch } from 'react-icons/fa';
+
+const usersData = [
+  { 
+    profile: 'https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+    username: 'John Doe' 
+  },
+  {
+    profile: 'https://images.unsplash.com/photo-1529068755536-a5ade0dcb4e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=881&q=80',
+    username: 'Midas Hofstra'
+  },
+  {
+    username: 'Yash Sehgal'
+  },
+  {
+    username: 'Elon Musk'
+  }
+]
 
 export default function App() {
   return (
     <div className="app" style={{
       fontFamily: 'Inter, sans-serif'
     }}>
-      <ReactSearchSmart />
+      <ReactSearchSmart children={
+        <React.Fragment>
+          <FaSearch />
+          <span>Search for users</span>
+        </React.Fragment>
+      } 
+      dataList={usersData}
+      inputPlaceholder="Search for users"
+      />
     </div>
   )
 }
--- src/component/ReactSearchSmart.js
+++ src/component/ReactSearchSmart.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Children, useState } from "react";
 import ReactModal from "react-modal";
 
 // stylings
@@ -6,6 +6,13 @@ import './component-styling/index.css';
 
 export default function ReactSearchSmart(componentProperties) {
     const [searchModalRef, setSearchModal] = useState(false);
+    const [inputPlaceholder] = useState(
+        componentProperties.inputPlaceholder ? componentProperties.inputPlaceholder : 'Search'
+    );
+    const [dataListRef, setDataList] = useState(
+        componentProperties.dataList ? componentProperties.dataList : []
+    );
+    const [currentSearchInputRef, setCurrentSearchInput] = useState('');
     return (
         <React.Fragment>
             <button className="rsscomponent_button"
@@ -14,13 +21,105 @@ export default function ReactSearchSmart(componentProperties) {
                     boxShadow: '0 1px 2px 0 rgba(0,0,0,0.2)',
                     fontWeight: '600',
                     fontFamily: 'inherit',
-                    border: '2px solid transparent'
+                    border: '2px solid transparent',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: '6px'
                 }}
-            >Search</button>
+                onClick={() => setSearchModal(true)}
+            >{componentProperties.children}</button>
             <ReactModal isOpen={searchModalRef}
                 onRequestClose={() => setSearchModal(false)}
+                style={{
+                    overlay: { 
+                        fontFamily: 'inherit',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    },
+                    content: { 
+                        fontFamily: 'inherit',
+                        width: '56%',
+                        marginRight: 'auto',
+                        marginLeft: 'auto',
+                        height: '400px',
+                        border: '1.2px solid #dddddd',
+                        boxShadow: '0 1px 2px 0 rgba(0,0,0,0.2)',
+                        padding: '1em'
+                    }
+                }}
             >
 
+                <input type="text" placeholder={inputPlaceholder} className="rssc_componentInput" 
+                    onChange={(searchInput) => {
+                        let currentSearchInput = searchInput.target.value;
+                        setCurrentSearchInput(currentSearchInput);
+                    }}
+                />
+                
+                <div className="data-list-wrapper">
+                    <ul className="data-list">
+                        {dataListRef.filter(item => {
+                            if (item === '') {
+                                return item;
+                            } else if (item.username.toLowerCase().includes(currentSearchInputRef.toLowerCase())) {
+                                return item;
+                            }
+                        })
+                        
+                        .map((dataItem, index) => (
+                            <li className="data-item" key={index}>
+                                {dataItem.username}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className="modal-action-buttons-wrapper_bottom"
+                    style={{
+                        position: 'absolute',
+                        bottom: '0',
+                        width: '96%',
+                        paddingBottom: '1.2em',
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'space-between'
+                    }}
+                >
+                    <button className="rsscomponent_button_close"
+                        style={{
+                            padding: '0.6em 1.2em',
+                            boxShadow: '0 1px 2px 0 rgba(0,0,0,0.2)',
+                            fontWeight: '600',
+                            fontFamily: 'Inter, sans-serif',
+                            border: '2px solid transparent',
+                            display: 'flex',
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            gap: '6px'
+                        }}
+                        onClick={() => setSearchModal(false)}
+                    >
+                        Close
+                    </button>
+                    <div className="powered-by-section" style={{
+                        width: 'fit-content',
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        gap: '6px',
+                        cursor: 'pointer'
+                    }}
+                    onClick={() => window.open('https://www.thedesignsystems.com/')}
+                    >
+                        <p style={{ color: '#dddddd', fontSize: '12px' }}>Powered by</p>
+                        <img src="https://www.github.com/DesignSystemsOSS.png" alt="tds-logo" 
+                            style={{ width: '36px', borderRadius: '50%' }}
+                        />
+                    </div>
+                </div>
             </ReactModal>
         </React.Fragment>
     )
