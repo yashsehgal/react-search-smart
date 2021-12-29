5335af7f65db75c4eb15be9fb00935366762f5bf
Yash Sehgal <yashsehgal.work@gmail.com>
2021-12-29 09:36:48 +0000

    style: inter font added from fonts.google.com

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import ReactSearchSmart from "./component/ReactSearchSmart";
 
 export default function App() {
   return (
-    <div className="app">
+    <div className="app" style={{
+      fontFamily: 'Inter, sans-serif'
+    }}>
       <ReactSearchSmart />
     </div>
   )
